0e84da15b083d858af6f78ff60eab2f2f250529d
inix84 <inix84@mail.ru>
2022-11-02 08:42:25 +0500

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void printIsLeapYear(int year) {
+        boolean evenYear = isLeapYear(year);
+        printIsLeapYearResult (year,evenYear);
     }
+    private static boolean isLeapYear(int year) {
+       return year % 4==0;
+    }
+    private static void printIsLeapYearResult(int year,boolean evenYear) {
+        if (evenYear) {
+            System.out.println("Год " + year + " високосный");
+        } else {
+            System.out.println("Год " + year + " не високосный");
+        }
+    }
+    public static void main(String[] args) {
+       int year =2000;
+        printIsLeapYear(year);
+        }
 }
